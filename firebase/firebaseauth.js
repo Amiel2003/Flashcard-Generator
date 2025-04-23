14aa605b9fb060d71a304c4878eefd05d1fb2c21
Amiel2003 <2101102216@student.buksu.edu.ph>
2025-04-23 15:50:35 +0800

    - remove firebase.json code

--- firebase/firebaseauth.js
+++ firebase/firebaseauth.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
-const admin = require('firebase-admin')
-const serviceAccount = require('./firebase.json')
+// require('dotenv').config()
+// const admin = require('firebase-admin')
+// const serviceAccount = require('./firebase.json')
 
-// set up credentials
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_URL
-})
+// // set up credentials
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: process.env.FIREBASE_URL
+// })
 
-const auth = admin.auth()
+// const auth = admin.auth()
 
-module.exports = auth
+// module.exports = auth
