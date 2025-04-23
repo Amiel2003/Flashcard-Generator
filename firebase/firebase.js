8c9c1e3c751647db421b585f33a1709c965a5f0d
Amiel2003 <2101102216@student.buksu.edu.ph>
2025-04-23 15:47:02 +0800

    - firebase serviceAccount base64 conversion

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,12 +1,18 @@
 require('dotenv').config()
 const admin = require('firebase-admin')
-const serviceAccount = require('./firebase.json')
 
-// set up credentials
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_URL
-})
+// Decode base64-encoded service account
+const serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICEACCOUNT_BASE64, 'base64').toString('utf8')
+);
+
+// Initialize Firebase Admin SDK
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: process.env.FIREBASE_URL,
+    });
+}
 
 const db = admin.database()
 
